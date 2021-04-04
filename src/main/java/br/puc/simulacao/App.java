c7b8f1fa3f4141d20de9b7c9ad84f4bb353dd610
Marcelocs19 <marcelo.home19@gmail.com>
2021-04-04 12:03:16 -0300

    Alterada versão do java

--- src/main/java/br/puc/simulacao/App.java
+++ src/main/java/br/puc/simulacao/App.java
@@ -4,10 +4,8 @@ package br.puc.simulacao;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
